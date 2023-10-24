602c0efdc2252bf5f7b4d70cea8e555bbbca240a
anvar <heyitsanvy@gmail.com>
2023-10-24 13:55:48 +0300

    tg-1: project init

---
+++ TheGrass/TheGrass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheGrass
+//
+//  Created by Anvar Rakhimov on 24.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheGrass/TheGrass/TheGrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheGrassApp.swift
+//  TheGrass
+//
+//  Created by Anvar Rakhimov on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheGrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
